b7d14c11d9dcf4b8f3f51984809a3a5737808d67
jydhasan <s1910933181@ru.ac.bd>
2021-02-17 21:30:05 +0600

    head

---
+++ index.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function(){
+$('#menu').click(function(){
+$('#col1').css({"position":"absolute","background-color":"#7FEBA3","display":"block"});
+});
+$('#close').click(function(){
+$('#col1').css({"display":"none"});
+});
+});
